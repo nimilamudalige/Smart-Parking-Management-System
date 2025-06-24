3879314a64544fd957c3fcf9d82ed9683ede74eb
unknown <nimilamudalige@gmail.com>
2025-06-25 01:23:16 +0530

    Initialize Eureka server

---
+++ Parking-Space-Service/src/test/java/com/example/parkingspace/ParkingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
